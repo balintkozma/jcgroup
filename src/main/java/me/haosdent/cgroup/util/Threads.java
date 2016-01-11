93b57cbee06351c214b308bdc42b92d312a76276
liuxinglanyue <liuxinglanyue@gmail.com>
2016-01-11 14:41:35 +0800

    delete call c

--- src/main/java/me/haosdent/cgroup/util/Threads.java
+++ src/main/java/me/haosdent/cgroup/util/Threads.java
@@ -1,10 +1,42 @@
 package me.haosdent.cgroup.util;
 
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Platform;
+
 public class Threads {
 
-  public static native int getThreadId();
+  private interface  CLibrary extends Library {
+    CLibrary INSTANCE = (CLibrary) Native.loadLibrary(Platform.isWindows() ? "msvcrt" : "c", CLibrary.class);
+    int syscall(int api);
+  }
+
+  private static final int __NR_gettid = Platform.isMac() ? 372 : (Platform.is64Bit() ? 186 : 224);
+
+  public static int getThreadId() {
+    return CLibrary.INSTANCE.syscall(__NR_gettid);
+  }
+
+  /*public static native int getThreadId();
 
   static {
     System.loadLibrary("Threads");
+  }*/
+
+  public static void main(String[] args) throws InterruptedException {
+    Thread thread = new Thread() {
+      @Override
+      public void run() {
+        int id = Threads.getThreadId();
+        System.out.println(id);
+        try {
+          Thread.sleep(100000000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    };
+    thread.start();
+    thread.join();
   }
 }
---
+++ src/test/java/me/haosdent/cgroup/util/JnaTest.java
@@ -0,0 +1,26 @@
+package me.haosdent.cgroup.util;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Platform;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by jiaojianfeng on 16/1/11.
+ */
+public class JnaTest {
+    private static final Logger LOG = LoggerFactory.getLogger(JnaTest.class);
+
+    @Test
+    public void testCLibrary() {
+        CLibrary.INSTANCE.printf("Hello World\n");
+    }
+
+    public interface CLibrary extends Library {
+        CLibrary INSTANCE = (CLibrary) Native.loadLibrary(Platform.isWindows() ? "msvcrt" : "c", CLibrary.class);
+
+        void printf(String format, Object... args);
+    }
+}
